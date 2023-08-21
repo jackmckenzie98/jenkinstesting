297cebee2841c3956f9f9086da4ceca0244f4aec
jackmckenzie98 <jack.mckenzie98@outlook.com>
2023-08-21 17:04:53 -0500

    Added additional calls for things like keys, refactoring of some code

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ import boto3
 base_url = "https://debian-pingfed:9999/pf-admin-api/v1"
 
 #Pull secret from secrets manager
-def get_secret():
-    secret_name = "debian-pf-api-secret"
+def get_secret(secret_name):
     region_name = "us-east-1"
     session = boto3.session.Session()
     client = session.client(
@@ -30,19 +29,24 @@ with open("apiCalls.json") as f:
     endpoint = json.load(f)
 
 session = requests.Session()
-secrets = get_secret()
+secrets = get_secret('debian-pf-api-secret')
 session.auth = (secrets["username"], secrets["pass"])
 session.headers.update({'X-XSRF-Header': 'PingFederate'})
+session.headers.update({'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br'})
 session.verify = False
 #Create a directory for the artifacts in the current working directory
 file_path = os.getcwd()
 final_path = os.path.join(file_path, r'artifactsPull')
+cert_path = os.path.join(final_path, r'certs')
 if not os.path.exists(final_path):
     os.makedirs(os.path.join(file_path, r'artifactsPull'))
+
+if not os.path.exists(cert_path):
+    os.makedirs(os.path.join(final_path, f'certs'))
 def make_calls():
     global clients, spConnections, authPolicies, idpAdapters, passwordCVs, accessTokenManagers, accessTokenMappings,\
-        authPolicyContracts, dataStores
-    get_secret()
+        authPolicyContracts, dataStores, keyPairs
+    get_secret('debian-pf-api-secret')
     clients = session.get(base_url + endpoint["oauthClients"]["endpoint"]).json()
     spConnections = session.get(base_url + endpoint["spConnections"]["endpoint"]).json()
     authPolicies = session.get(base_url + endpoint["authPolicies"]["endpoint"]).json()
@@ -52,16 +56,18 @@ def make_calls():
     accessTokenMappings = session.get(base_url + endpoint["accessTokenMappings"]["endpoint"]).json()
     authPolicyContracts = session.get(base_url + endpoint["authPolicyContracts"]["endpoint"]).json()
     dataStores = session.get(base_url + endpoint["dataStores"]["endpoint"]).json()
+    keyPairs = session.get(base_url + endpoint["keyPairs"]["endpoint"]).json()
 
 def write_to_file():
     list_thing = [clients, spConnections, authPolicies, idpAdapters, passwordCVs, accessTokenManagers,
-                  accessTokenMappings, authPolicyContracts, dataStores]
+                  accessTokenMappings, authPolicyContracts, dataStores, keyPairs]
     file_names = ["clients", "spConnections", "authPolicies", "idpAdapters", "passwordCredentialValidators",
-                  "accessTokenManagers", "accessTokenMappings", "authPolicyContracts", "dataStores"]
+                  "accessTokenManagers", "accessTokenMappings", "authPolicyContracts", "dataStores", "keyPairs"]
     for name, obj in zip(file_names, list_thing):
         f = open(f"{final_path}/{name}.json", 'w+')
         f.write(json.dumps(obj, indent=3))
         f.close()
+
 make_calls()
 write_to_file()
 
